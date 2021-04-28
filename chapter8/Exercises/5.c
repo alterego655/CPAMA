be727f687186156a02e63ab26951b19fb0683768
Xuneng Zhou <824662526@qq.com>
2021-04-28 15:58:36 +0800

    some changes

---
+++ chapter8/Exercises/5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int fib_numbers[40] = {0, 1};
+    int i;
+
+    for (i = 2; i < sizeof(fib_numbers) / sizeof(fib_numbers[0]); i++) {
+            
+        fib_numbers[i] = fib_numbers[i-1] + fib_numbers[i-2];
+
+    }
+    
+    return 0;
+}
---
+++ chapter8/Projects/1.c
@@ -0,0 +1,30 @@
+/* Checks numbers for repeated digits */
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void)
+{
+    bool digit_seen[10] = {false}, repeated[10] = {false};
+    int digit, i;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+    
+    while (n > 0) {
+        digit = n % 10;
+        if (digit_seen[digit])
+            repeated[digit] = true;
+        digit_seen[digit] = true;
+         n /= 10;
+    }
+
+    printf("Repeated digits(s): ");
+
+    for (i = 0; i < 10; i++) {
+        if (repeated[i])
+            printf("%d", i);
+    }
+    printf("\n");
+    return 0;
+}

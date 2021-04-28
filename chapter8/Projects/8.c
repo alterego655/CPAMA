4926eddfb07643aea01a831852af79c54e76acfd
Xuneng Zhou <824662526@qq.com>
2021-04-28 16:06:15 +0800

    some projects

--- chapter8/Projects/8.c
+++ chapter8/Projects/8.c
@@ -2,37 +2,45 @@
 
 int main(void)
 {
-    int i, j, temp_total;
+    int i, j, temp_total, high, low;
     int n[5][5];
 
     for (i = 0; i < 5; i++) {
-        printf("Enter row %d: ", i + 1);
+        printf("Enter quiz grades for student  %d: ", i + 1);
     
         for (j = 0; j < 5; j++) {
             scanf("%d", &n[i][j]);
         }
     }
 
-    printf("Row totals: ");
+    printf("\n");
 
     for (i = 0; i < 5; i++) {
+        printf("\nTotal and average score for student %d: ", i + 1); 
         temp_total = 0;
         for (j = 0; j < 5; j++) {
             temp_total += n[i][j];
         }
-        printf("%d ", temp_total);
+        printf("%d %.2f", temp_total, (double) temp_total / 5);
     }
 
-    printf("\nColumn totals: ");
+    printf("\n");
 
     for (i = 0; i < 5; i++) {
+        printf("\nAverage, high and low scores for quiz %d: ", i + 1);
         temp_total = 0;
+        high = low = n[0][i];
+        
         for (j = 0; j < 5; j++) {
             temp_total += n[j][i];
+            if (n[j][i] > high)
+                high = n[j][i];
+            if (n[j][i] < low)
+                low = n[j][i];
         }
-        printf("%d ", temp_total);
+        printf("%f %d %d", (double) temp_total / 5, high, low);
     }
-    
+
     printf("\n");
 
     return 0;
